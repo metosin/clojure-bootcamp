a605c4628cbabf2ba8b87cd7f919c11185a50bc9
Tommi Reiman <tommi.reiman@metosin.fi>
2013-10-07 09:31:47 +0300

    ide settings

--- project.clj
+++ project.clj
@@ -4,4 +4,6 @@
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.5.1"]]
   :profiles {:dev {:dependencies [[midje "1.5.1"]]
-                   :plugins [[lein-midje "3.1.1"]]}})
+                   :plugins [[lein-midje "3.1.1"]
+ 							 [lein2-eclipse "2.0.0"]
+                             [lein-idea "1.0.1"]]}})
