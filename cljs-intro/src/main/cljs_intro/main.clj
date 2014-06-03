341982786417224b1c32ab9f2a215b6a472508bd
jarppe <jarppe@metosin.fi>
2014-06-03 17:34:02 +0300

    fixed ns

--- cljs-intro/src/main/cljs_intro/main.clj
+++ cljs-intro/src/main/cljs_intro/main.clj
@@ -1,6 +1,7 @@
-(ns misdef.main
+(ns cljs-intro.main
   (:gen-class))
 
 (defn -main [& args]
   (require 'cljs-intro.server.server)
-  (apply (resolve 'cljs-intro.server.server/run) args))
+  (apply (resolve 'cljs-intro.server.server/run) args)
+  (println "Server running..."))
