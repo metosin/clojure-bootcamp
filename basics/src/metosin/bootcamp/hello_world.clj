efafbdb10aaba1ffbb4784b839d56594b5da9a23
jarppe <jarppe@metosin.fi>
2014-06-03 12:11:15 +0300

    cleanup

--- basics/src/metosin/bootcamp/hello_world.clj
+++
@@ -1,6 +0,0 @@
-(ns metosin.bootcamp.hello-world)
-
-(defn hello-world [your-name]
-  (println "Hello," your-name))
-
-(hello-world "world!")
--- basics/test/metosin/clojure_basics_test.clj
+++
@@ -1,6 +0,0 @@
-(ns metosin.clojure-basics-test
-  (:require [midje.sweet :refer :all]
-            [metosin.clojure-basics :refer :all]))
-
-(fact
-  (+ 1 2) => 3)
