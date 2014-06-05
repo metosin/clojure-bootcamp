2174dcb2e7b8424a03635de7323bfc3d71b2cbbd
jarppe <jarppe@metosin.fi>
2014-06-05 08:03:42 +0300

    cleanup

--- working-with-data/test/working_with_data_test.clj
+++ working-with-data/test/working_with_data_test.clj
@@ -2,6 +2,10 @@
   (:require [working-with-data :refer :all]
             [midje.sweet :refer :all]))
 
+(facts average-experience
+  (average-experience [{:prog-years 5} {:prog-years 7}]) => 6
+  (average-experience [{:prog-years 5} {:prog-years 7} {:prog-years nil}]) => 6)
+
 (tabular
   (fact knows-lang
     (knows-lang ?data ?lang) => ?num)
@@ -10,7 +14,3 @@
     :foo    1      [{:lang :foo}]
     :foo    2      [{:lang :foo} {:lang :foo}]
     :foo    1      [{:lang :foo} {:lang :bar}])
-
-(facts average-experience
-  (average-experience [{:prog-years 5} {:prog-years 7}]) => 6
-  (average-experience [{:prog-years 5} {:prog-years 7} {:prog-years nil}]) => 6)
