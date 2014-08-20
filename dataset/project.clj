527fdf49bbfa42a294be69449a83a2b5519beb97
jarppe <jarppe@metosin.fi>
2014-08-20 10:17:27 +0300

    Initiail dataset

---
+++ dataset/project.clj
@@ -0,0 +1,2 @@
+(defproject metosin.clojure-bootcamp/dataset "0.1.0"
+  :dependencies [[org.clojure/clojure "1.6.0"]])
---
+++ dataset/src/metosin/dataset/pizzas.clj
@@ -0,0 +1,15 @@
+(ns metosin.dataset.pizzas)
+
+(def pizzaz [{:name "Alla Pollo"
+              :price    8.30
+              :toppings {:onion 2
+                         :ham   3}}
+             {:name "Americana"
+              :price    7.90
+              :toppings {:pineaple    2
+                         :ham         3
+                         :blue-cheese 1}}
+             {:name "Margherita"
+              :price    7.20
+              :toppings {:mozarella 4
+                         :tomato    3}}])
