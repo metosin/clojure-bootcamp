e4dd7a1e5112e9a1a68c693726bdb82148b09a31
Juho Teperi <juho.teperi@iki.fi>
2014-08-20 12:46:55 +0300

    git status

--- working-with-data/src/working_with_data.clj
+++ working-with-data/src/working_with_data.clj
@@ -1,12 +1,4 @@
 (ns working-with-data
-  (:require [working-with-data.data :refer [participants]]))
+  (:require [metosin.dataset.pizzas :refer [pizzas]]))
 
-(defn knows-lang [participants lang]
-  ; Return the number of paritcipants that have reported knowledge
-  ; on language 'lang'
-  )
-
-(defn average-experience [participants]
-  ; Return the average work experience of given participants. Note that
-  ; some participants have not reported work experience.
-  )
+(println pizzas)
